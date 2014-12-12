3ef65b3a2f2668e2f6ec07b4d9b4a0c10ffea652
unknown <mfl3@cin.ufpe.br>
2014-12-12 21:58:05 -0200

    Povoamento da tabela de pagamentos

---
+++ Pagamento.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_pagamento VALUES(tp_pagamento(1004, 'Credito', TO_DATE('12/12/2014', 'dd/mm/yyyy'), 765.80));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(10, 'Credito', TO_DATE('15/11/2014', 'dd/mm/yyyy'), 674.00));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(1002, 'A vista', TO_DATE('18/10/2014', 'dd/mm/yyyy'), 235.50));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(4, 'Boleto', TO_DATE('19/11/2014', 'dd/mm/yyyy'), 321.80));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(14, 'Deposito', TO_DATE('03/12/2014', 'dd/mm/yyyy'), 500.90));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(124, 'Credito', TO_DATE('27/12/2014', 'dd/mm/yyyy'), 150.10));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(10334, 'A vista', TO_DATE('09/10/2014', 'dd/mm/yyyy'), 750.20));
+INSERT INTO tb_pagamento VALUES(tp_pagamento(9422334, 'Deposito', TO_DATE('01/11/2014', 'dd/mm/yyyy'), 343.20));
