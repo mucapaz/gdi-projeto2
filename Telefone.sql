7c435321368433c8aa0ded8b7b076423a87fd52b
unknown <mfl3@cin.ufpe.br>
2014-12-12 21:18:26 -0200

    Povoamento da tabela de telefones.

---
+++ Telefone.sql
@@ -0,0 +1,29 @@
+--Telefones de Lojas
+
+INSERT INTO tb_telefone VALUES(tp_telefone('3943-4022'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3243-2022'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3193-1022'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3503-5022'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3483-6029'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3373-1022'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3353-0027'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3243-4032'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3143-5025'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3343-5021'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3797-5785'));
+INSERT INTO tb_telefone VALUES(tp_telefone('3123-5462'));
+
+-- Telefones de Funcionários
+
+INSERT INTO tb_telefone VALUES(tp_telefone('00000001'));
+INSERT INTO tb_telefone VALUES(tp_telefone('33419110'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000003'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000004'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000005'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000006'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000007'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000008'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000009'));
+INSERT INTO tb_telefone VALUES(tp_telefone('00000000'));
+
+
