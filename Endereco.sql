d0eccf5adb33800322de2f557600bba2e5714df7
pazspm <pazspm@users.noreply.github.com>
2014-12-12 21:34:14 -0300

    Update Endereco.sql

--- Endereco.sql
+++ Endereco.sql
@@ -1,33 +1,27 @@
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('1', 'Paraiba', '1', 'Menino', 'Boa', 'Hellcity');
+INSERT INTO tb_endereco VALUES (tp_endereco ('1', 'Paraiba', '1', 'Menino', 'Boa', 'Hellcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('2', 'Pernambuco', '2', 'Coisa', 'Viagem', 'Relcity');
+INSERT INTO tb_endereco VALUES (tp_endereco('2', 'Pernambuco', '2', 'Coisa', 'Viagem', 'Relcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('3', 'Paraiba', '3', 'Menino', 'Viagem', 'Relcity');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('3', 'Paraiba', '3', 'Menino', 'Viagem', 'Relcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('4', 'Paraiba', '4', 'Menino', 'Boa', 'Hellcity');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('4', 'Paraiba', '4', 'Menino', 'Boa', 'Hellcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ( '5', 'Paraiba', '5', 'Menino', 'Boa', 'Hellcife');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('5', 'Paraiba', '5', 'Menino', 'Boa', 'Hellcife'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('6', 'Pernambuco', '6', 'Danado', 'Boa', 'Relcity');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('6', 'Pernambuco', '6', 'Danado', 'Boa', 'Relcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('7', 'Pernambuco', '7', 'Menino', 'Boa', 'Hellcife');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('7', 'Pernambuco', '7', 'Menino', 'Boa', 'Hellcife'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('Marlon', 'Lima', '8', 'Bahia', '8', 'Menino', 'Viagem', 'Hellcife');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('Marlon', 'Lima', '8', 'Bahia', '8', 'Menino', 'Viagem', 'Hellcife'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('9', 'Bahia', '9', 'Coisa', 'Viagem', 'Relcity');
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('9', 'Bahia', '9', 'Coisa', 'Viagem', 'Relcity'));
 
-INSERT INTO tb_endereco (CEP, Estado, Numero, Logradouro, Bairro, Cidade)
-VALUES ('10', 'Paraiba', '10', 'Danado', 'Rosa', 'Hellcife');
-
-
-
-
+INSERT INTO tb_endereco  
+VALUES (tp_endereco('10', 'Paraiba', '10', 'Danado', 'Rosa', 'Hellcife'));
