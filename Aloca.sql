49cf6dda9174cc5426f46952f3f7d15feb761cb9
unknown <mfl3@cin.ufpe.br>
2014-12-13 02:44:13 -0200

    Povoamento da tabela tb_aloca.

---
+++ Aloca.sql
@@ -0,0 +1,35 @@
+INSERT INTO tb_aloca VALUES (
+	tp_aloca (
+		(SELECT REF(I) FROM tb_interno I WHERE I.CPF = '000.000.000-1'),
+		(SELECT REF(A) FROM tb_area A WHERE A.Codigo = 1),
+		(SELECT REF(E) FROM tb_evento E WHERE E.Codigo = 1)
+	)
+);
+INSERT INTO tb_aloca VALUES (
+	tp_aloca (
+		(SELECT REF(I) FROM tb_interno I WHERE I.CPF = '000.000.000-2'),
+		(SELECT REF(A) FROM tb_area A WHERE A.Codigo = 2),
+		(SELECT REF(E) FROM tb_evento E WHERE E.Codigo = 3)
+	)
+);
+INSERT INTO tb_aloca VALUES (
+	tp_aloca (
+		(SELECT REF(I) FROM tb_interno I WHERE I.CPF = '000.000.000-3'),
+		(SELECT REF(A) FROM tb_area A WHERE A.Codigo = 3),
+		(SELECT REF(E) FROM tb_evento E WHERE E.Codigo = 2)
+	)
+);
+INSERT INTO tb_aloca VALUES (
+	tp_aloca (
+		(SELECT REF(I) FROM tb_interno I WHERE I.CPF = '000.000.000-4'),
+		(SELECT REF(A) FROM tb_area A WHERE A.Codigo = 4),
+		(SELECT REF(E) FROM tb_evento E WHERE E.Codigo = 5)
+	)
+);
+INSERT INTO tb_aloca VALUES (
+	tp_aloca (
+		(SELECT REF(I) FROM tb_interno I WHERE I.CPF = '000.000.000-5'),
+		(SELECT REF(A) FROM tb_area A WHERE A.Codigo = 5),
+		(SELECT REF(E) FROM tb_evento E WHERE E.Codigo = 6)
+	)
+);
