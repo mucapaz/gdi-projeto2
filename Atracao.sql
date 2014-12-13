7fd4ee6d7225fe0d92c722e79e2e9db51c78367a
pazspm <pazspm@users.noreply.github.com>
2014-12-12 21:36:34 -0300

    Update Atracao.sql

--- Atracao.sql
+++ Atracao.sql
@@ -1,20 +1,20 @@
-INSERT INTO tb_atracao (Nome, Tema, Tipo, Descricao)
-VALUES ('Banda Polentinha do Arrocha', 'Axé', 'Música', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur blandit arcu eget laoreet. Etiam auctor massa ac urna condimentum semper. ');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Banda Polentinha do Arrocha', 'Axé', 'Música', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur blandit arcu eget laoreet. Etiam auctor massa ac urna condimentum semper. '));
 
-INSERT INTO tb_atracao ( Nome, Tema, Tipo, Descricao)
-VALUES ( 'Palhaço Chocolate', 'Brincadeiras', 'Humor', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Palhaço Chocolate', 'Brincadeiras', 'Humor', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.'));
 
-INSERT INTO tb_atracao ( Nome, Tema, Tipo, Descricao)
-VALUES ('Victor e Leo', 'É festa de rodeio', 'Música', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Victor e Leo', 'É festa de rodeio', 'Música', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.'));
 
-INSERT INTO tb_atracao ( Nome, Tema, Tipo, Descricao)
-VALUES ('Marlon Marley', 'Como tocar cuíca', 'Workshop', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non lacus nibh. Morbi euismod sodales ligula auctor pretium.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Marlon Marley', 'Como tocar cuíca', 'Workshop', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non lacus nibh. Morbi euismod sodales ligula auctor pretium.'));
 
-INSERT INTO tb_atracao ( Nome, Tema, Tipo, Descricao)
-VALUES ( 'DJ Samuel', 'Party EletroPetro', 'Música', 'Phasellus lacinia nibh et elit sollicitudin vehicula. Proin et nunc lobortis mauris tempus tincidunt. Sed dictum congue nunc, ac lobortis lacus sodales id.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'DJ Samuel', 'Party EletroPetro', 'Música', 'Phasellus lacinia nibh et elit sollicitudin vehicula. Proin et nunc lobortis mauris tempus tincidunt. Sed dictum congue nunc, ac lobortis lacus sodales id.'));
 
-INSERT INTO tb_atracao (Nome, Tema, Tipo, Descricao)
-VALUES ( 'Danilo Gentily', 'Eleições', 'Stand-Up', 'Fusce urna nibh, ultricies semper tellus eu, dignissim volutpat odio. Aenean aliquet, felis interdum semper consectetur, turpis tellus feugiat erat.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Danilo Gentily', 'Eleições', 'Stand-Up', 'Fusce urna nibh, ultricies semper tellus eu, dignissim volutpat odio. Aenean aliquet, felis interdum semper consectetur, turpis tellus feugiat erat.'));
 
-INSERT INTO tb_atracao ( Nome, Tema, Tipo, Descricao)
-VALUES ( 'Shevchenko e Elloco', 'Brega de cada dia', 'Música', 'Ultricies semper tellus eu, dignissim volutpat odio.');
+INSERT INTO tb_atracao 
+VALUES (tp_atracao( 'Shevchenko e Elloco', 'Brega de cada dia', 'Música', 'Ultricies semper tellus eu, dignissim volutpat odio.'));
