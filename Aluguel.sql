1fac52641a786f5d549eebec5f513608959fc1f5
unknown <mfl3@cin.ufpe.br>
2014-12-12 23:14:36 -0200

    Povoamento da tabela de aluguéis.

---
+++ Aluguel.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_aluguel VALUES(tp_aluguel(1, TO_DATE('05/11/2014', 'dd/mm/yyyy'), 1194.65, 'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(2, TO_DATE('06/12/2014', 'dd/mm/yyyy') , 608.6800000001, 'fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(3, TO_DATE('15/11/2014', 'dd/mm/yyyy'), 1812.11, 'nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(4, TO_DATE('13/12/2014', 'dd/mm/yyyy'), 2444.23, 'mi nulla ac enim in tempor turpis nec euismod scelerisque'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(5, TO_DATE('01/01/2015', 'dd/mm/yyyy'), 900.91, 'pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(6, TO_DATE('05/01/2015', 'dd/mm/yyyy'), 538.33, 'amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(7, TO_DATE('17/12/2014', 'dd/mm/yyyy'), 962.74, 'quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(8, TO_DATE('20/11/2014', 'dd/mm/yyyy'), 9355.11, 'pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut erat id mauris vulputate'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(9, TO_DATE('23/11/2014', 'dd/mm/yyyy'), 1512.81, 'posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in'));
+INSERT INTO tb_aluguel VALUES(tp_aluguel(10, TO_DATE('24/10/2014', 'dd/mm/yyyy'), 1277.01, 'magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque'));
