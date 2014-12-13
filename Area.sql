9cf7414611c34f668bf3dae0c6a2c3ab99ddb0ed
unknown <mfl3@cin.ufpe.br>
2014-12-12 22:35:48 -0200

    Povoamento da tabela de áreas

---
+++ Area.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_area VALUES(tp_area(1, 'Externa', ' Etiam justo. Etiam pretium iaculis justo..', (SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 74.18 AND T.comprimento = 20.9 AND T.largura = 40.61)));
+INSERT INTO tb_area VALUES(tp_area(2, 'Mista', ' ut volutpat sapien arcu sed augue..',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 25.83 AND T.comprimento = 26.86 AND T.largura = 19.64)));
+INSERT INTO tb_area VALUES(tp_area(3, 'Mista', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 70.47 AND T.comprimento = 4.43 AND T.largura = 45.65)));
+INSERT INTO tb_area VALUES(tp_area(4, 'Externa', ' Etiam faucibus cursus urna. Ut tellus.  Nullam varius.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 81.34 AND T.comprimento = 86.67 AND T.largura = 97.09)));
+INSERT INTO tb_area VALUES(tp_area(5, 'Interna', '  turpis enim blandit mi, in porttitor pede justo eu massa.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 60.69 AND T.comprimento = 32.25 AND T.largura = 87.99)));
+INSERT INTO tb_area VALUES(tp_area(6, 'Mista', '  Integer ac leo. Pellentesque ultrices mattis odio.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 77.11 AND T.comprimento = 64.44 AND T.largura = 60.45)));
+INSERT INTO tb_area VALUES(tp_area(7, 'Externa', ' nibh ligula nec sem.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 68.75 AND T.comprimento = 51.31 AND T.largura = 38.62)));
+INSERT INTO tb_area VALUES(tp_area(8, 'Interna', 'erra dapibus. Nulla suscipit ligula in lacus.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 93.67 AND T.comprimento = 2.44 AND T.largura = 6.19)));
+INSERT INTO tb_area VALUES(tp_area(9, 'Interna', 'llis molestie lorem.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 67.42 AND T.comprimento = 37.3 AND T.largura = 85.53)));
+INSERT INTO tb_area VALUES(tp_area(10, 'Mista', 'Praesent blandit. Nam nulla.',(SELECT REF(T) FROM tb_tamanho T WHERE T.altura = 93.18 AND T.comprimento = 91.7 AND T.largura = 8.43)));
